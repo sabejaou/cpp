afa92f7086f76f9c3454dfd419658cefb418ae1b
sabejaou <sabejaou@student.42lyon.fr>
2024-09-24 20:12:22 +0200

    cpp07 two first

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -2,7 +2,7 @@
 #include <string>
 
 template <typename T>
-void iter(T *array, size_t length, void (*f)(T const &)) {
+void iter(T *array, size_t length, void (*f)(T &)) {
 	for (size_t i = 0; i < length; i++) {
 		f(array[i]);
 	}
